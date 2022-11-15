303f577a7223466e25597312f65a0f5686035506
Watthachai Taechalue <wattchaichai@gmail.com>
2022-11-15 16:47:59 +0700

    Update miniproject.py
    this is a commit to my friend
    
    Co-Authored-By: Microvave <118260391+Microvave@users.noreply.github.com>

--- miniproject.py
+++ miniproject.py
@@ -3,3 +3,5 @@ import requests
 r = requests.get('https://opend.data.go.th/get-ckan/datastore_search?resource_id=3cea9bc0-df36-4b36-b46a-7bbc206b36dd&limit=5&q=title:jones')
 print(r)
 print(dir(r))
+
+""" I want to make a bar graph"""
